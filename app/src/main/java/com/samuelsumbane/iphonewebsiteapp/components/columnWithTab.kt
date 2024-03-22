51fa8fbe3753fed88d5c9d1bb20fd2e2966d1ad8
samuelsumbane <samuelsumbane143@gmail.com>
2024-03-22 08:57:11 +0200

    code improving - 22.03.24 - 08:57

--- app/src/main/java/com/samuelsumbane/iphonewebsiteapp/components/columnWithTab.kt
+++ app/src/main/java/com/samuelsumbane/iphonewebsiteapp/components/columnWithTab.kt
@@ -4,19 +4,24 @@ package com.samuelsumbane.iphonewebsiteapp.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -35,42 +40,112 @@ import com.samuelsumbane.iphonewebsiteapp.R
 @Composable
 fun ColumnWithTab(){
 
+    val minSize = 70
+    val maxSize = 200
+    var columnState1 = "open" // Define column state, if is closed or open.
+    var columnState2 = "open" // Define column state, if is closed or open.
+
+
+    var size1 = 1 // first row inicial size
+    var size2 = 1 // second row inicial size
+    var size3 = 1 // third row inicial size
+
+    if(columnState1 == "open"){
+        size1 = maxSize
+        size2 = minSize
+        size3 = minSize
+    }else if(columnState2 == "open"){
+        size1 = minSize
+        size2 = maxSize
+        size3 = minSize
+    }else{
+        size1 = minSize
+        size2 = minSize
+        size3 = maxSize
+    }
+
+
     Column(
         modifier = Modifier
             .width(250.dp)
-            .height(500.dp)
+//            .height(300.dp)
             .background(Lightgray, divShapes.medium)
     ){
-        Column(modifier = Modifier
+        Row(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(0.4f),
-//            .background(Color.Blue),
-            horizontalAlignment = Alignment.CenterHorizontally
+            .height(size1.dp)
+            .background(Color.Blue, divShapes.medium)
         ) {
-            Spacer(Modifier.height(30.dp))
-            Text("dsafd", fontWeight = FontWeight.Bold)
-            Spacer(Modifier.height(20.dp))
+            Column(
+                Modifier.fillMaxWidth(0.85f)
+                    .fillMaxHeight().background(Color.Red, divShapes.medium)
+            ){
+
+            }
+            //
+            IconButton(onClick = {
+                    println("clicked 1 one")
+                },
+                Modifier.align(Alignment.CenterVertically)
+            ){
+                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+            }
         }
+
         Divider(
-            color = Color.DarkGray,
-            thickness = 1.5.dp,
+            color = Color.DarkGray, thickness = 1.5.dp,
             modifier = Modifier.fillMaxWidth(0.8f).align(Alignment.CenterHorizontally).padding(vertical = 7.dp)
         )
-        Column(modifier = Modifier
+
+
+        Row(modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(1f)
-            .background(Color.Transparent, divShapes.medium),
-            horizontalAlignment = Alignment.CenterHorizontally
+            .height(size2.dp)
+            .background(Color.Transparent, divShapes.medium)
         ){
-            Image(
-                painter = painterResource(id = R.drawable.airpods__eb24cvhoe26a_small),
-                contentDescription = "divImage",
-                modifier = Modifier
-                    .width(190.dp)
-                    .height(280.dp),
-                contentScale = ContentScale.Crop
-            )
+            Column(
+                Modifier.fillMaxWidth(0.85f)
+                    .fillMaxHeight().background(Color.Red, divShapes.medium)
+            ){
+
+            }
+            //
+            IconButton(onClick = {
+                println("clicked 1 one")
+            },
+                Modifier.align(Alignment.CenterVertically)
+            ){
+                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+            }
         }
+
+        Divider(
+            color = Color.DarkGray, thickness = 1.5.dp,
+            modifier = Modifier.fillMaxWidth(0.8f).align(Alignment.CenterHorizontally).padding(vertical = 7.dp)
+        )
+
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .height(size3.dp)
+            .background(Color.Transparent, divShapes.medium)
+        ){
+            Column(
+                Modifier.fillMaxWidth(0.85f)
+                    .fillMaxHeight().background(Color.Red, divShapes.medium)
+            ){
+
+            }
+            //
+            IconButton(onClick = {
+                println("clicked 1 one")
+            },
+                Modifier.align(Alignment.CenterVertically)
+            ){
+                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+            }
+        }
+
     }
 }
 
